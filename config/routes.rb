315ef8e392a53ba156ac9559f0692db43d5564d2
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-18 13:54:31 -0700

    added notes and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # get 'welcome/index'
   
-  resources :articles
+  # This creates comments as a nested resource within articles. This is another part of capturing the hierarchical relationship that exists between articles and comments.
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
